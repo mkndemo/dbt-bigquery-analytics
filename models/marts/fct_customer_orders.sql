f87e11c33762e1b952b226c81cb404bcf249f8e9
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-09-18 14:52:00 +0000

    test

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -15,7 +15,7 @@ customer_orders as (
     select
         customer_id,
 
-        min(order_date) as first_order_date,
+        MAX(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
